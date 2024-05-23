642fd2a188546e20c7c90d55b757d9871dbb278f
Josef Zoller <josef@walterzollerpiano.com>
2024-05-23 17:45:26 +0200

    Add sdk usage example

--- sdks/python/p2pindustries/example_usage.py
+++ sdks/python/p2pindustries/example_usage.py
@@ -1,80 +1,44 @@
 import asyncio
 import logging
-from service import P2PConnection, RequestResponseService, DiscoveryService, GossipSubService, DHTService, FileTransferService
+from p2p_sdk import P2PConnection, RequestResponseService, DiscoveryService
+from typing import Optional
 
+async def get_first_neighbour(service: DiscoveryService) -> Optional[str]:
+    async with service.discovery_events() as events:
+        async for event in events:
+            if event.is_init():
+                return event.init().peer_id
+    return None
 
-##########------ USER DEFINED SCRIPT ------##########
-from gpiozero import Button, LED
+async def handle_requests(
+    service: RequestResponseService,
+    topic: Optional[str] = None,
+    regex: bool = False
+):
+    async with service.receive_requests(topic, regex) as requests:
+        async for request in requests:
+            print(request)
+            await request.send_response(b'Hello from the other side!')
 
-led = LED(17)
-switch = Button(27)
+async def main():
+    async with P2PConnection() as connection:
+        discovery = connection.discovery_service()
+        req_resp = connection.request_response_service()
 
-queue = asyncio.Queue()
+        peer_id = await get_first_neighbour(discovery)
 
-def handle_switch(request_queue, loop):
-    def put_request(data):
-        loop.call_soon_threadsafe(request_queue.put_nowait, data)
-    
-    switch.when_pressed = lambda: put_request("ON")
-    switch.when_released = lambda: put_request("OFF")
+        if peer_id is None:
+            logging.error("No peers found")
+            return
 
-async def request_worker(queue, requestResponseService: RequestResponseService, peer_id, topic):
-    while True:
-        data = await queue.get()
-        response = requestResponseService.send_request(peer_id, data, topic)
-        queue.task_done()
+        logging.info(f"Found peer: {peer_id}")
 
-async def receive_respond(requestResponseService: RequestResponseService):
-    async for receive_message in requestResponseService.receive_generator():
-        request_data = receive_message.data.decode()
-        if request_data == "ON":
-            led.on()
-        elif request_data == "OFF":
-            led.off()
-        requestResponseService.respond(0, "OK")
+        asyncio.create_task(handle_requests(req_resp))
+        asyncio.create_task(handle_requests(req_resp, topic='foo'))
+        asyncio.create_task(handle_requests(req_resp, topic=r'bar[0-9]+', regex=True))
 
-async def init_neighbours(discovery_service: DiscoveryService):
-    discovery_service.discover()
-    return discovery_service.neighbours_peerIDs
+        response = await req_resp.send_request(peer_id, b'Hello!', topic='topic1')
 
-# TODO: Let the API handle this automatically
-async def maintain_current_neighbours(discovery_service: DiscoveryService):
-    init_neighbours = init_neighbours(discovery_service)
-    while True:
-        discovery_service.subscribe_events()
-
-# every script uses an async run() function
-async def run() -> None:
-    channel = P2PConnection()
-    event_loop = asyncio.get_event_loop()
-
-    requestResponseService = RequestResponseService(channel)
-    discoveryService = DiscoveryService(channel)
-    gossipSubService = GossipSubService(channel)
-    dhtService = DHTService(channel)
-    fileTransferService = FileTransferService(channel)
-    
-
-    handle_switch(queue, event_loop)
-
-
-    receive_respond_task = asyncio.create_task(receive_respond(requestResponseService))
-
-    # TODO: Figure this stuff out
-    request_worker_task = []
-    for i, neighbour_peer_id in enumerate(discoveryService.neighbours_peerIDs):
-        for j, Topic in enumerate(gossipSubService.subscriptios):
-            topic = Topic.topic
-            request_worker_task.append(asyncio.create_task(request_worker(queue, requestResponseService, neighbour_peer_id, topic)))
-
-    await receive_respond_task
-
-    # TODO: Obviously this doesn't work
-    for i, _ in enumerate(discoveryService.neighbours_peerIDs):
-        for j, _ in enumerate(gossipSubService.subscriptions):
-            await request_worker_task[i]
-    
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    asyncio.run(run())
+    asyncio.run(main())
