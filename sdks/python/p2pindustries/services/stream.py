8939e5ea409a00ac09b3a83230bd5772ee1eabc2
Lukas Altun <laltun@student.ethz.ch>
2024-05-24 16:32:59 +0200

    fix stream cancelling

--- sdks/python/p2pindustries/services/stream.py
+++ sdks/python/p2pindustries/services/stream.py
@@ -1,3 +1,6 @@
+import grpc
+
+
 class ManagedStream:
     def __init__(self, stream):
         self.stream = stream
@@ -7,6 +10,12 @@ class ManagedStream:
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         self.stream.cancel()
+
+        try:
+            await self.stream
+        except grpc.aio.AioRpcError as e:
+            if e.code() != grpc.StatusCode.CANCELLED:
+                print(f'Stream encountered an error: {e}')
 
     def __aiter__(self):
         return self
