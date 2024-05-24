3fd9e9987e2686e57d833a22b7f08ea0ffb54f99
Linus Meierhoefer <linusmeierhoefer@protonmail.com>
2024-05-24 11:19:38 +0200

    update compiles protobuf

--- sdks/python/p2pindustries/protocol/script_pb2.py
+++ sdks/python/p2pindustries/protocol/script_pb2.py
@@ -3,7 +3,6 @@
 # source: script.proto
 # Protobuf Python Version: 5.26.1
 """Generated protocol buffer code."""
-
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import symbol_database as _symbol_database
@@ -13,71 +12,71 @@ from google.protobuf.internal import builder as _builder
 _sym_db = _symbol_database.Default()
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
-    b'\n\x0cscript.proto\x12\x06script"\x07\n\x05\x45mpty"\x12\n\x02ID\x12\x0c\n\x04ulid\x18\x01 \x02(\t"\x16\n\x05Topic\x12\r\n\x05topic\x18\x01 \x02(\t"-\n\rOptionalTopic\x12\x1c\n\x05topic\x18\x01 \x01(\x0b\x32\r.script.Topic"F\n\nTopicQuery\x12\x1e\n\x05topic\x18\x01 \x01(\x0b\x32\r.script.TopicH\x00\x12\x0f\n\x05regex\x18\x02 \x01(\tH\x00\x42\x07\n\x05query"7\n\x12OptionalTopicQuery\x12!\n\x05query\x18\x01 \x01(\x0b\x32\x12.script.TopicQuery"=\n\x07Message\x12\x0c\n\x04\x64\x61ta\x18\x01 \x02(\x0c\x12$\n\x05topic\x18\x02 \x02(\x0b\x32\x15.script.OptionalTopic"<\n\x0bSendRequest\x12\x0f\n\x07peer_id\x18\x01 \x02(\t\x12\x1c\n\x03msg\x18\x02 \x02(\x0b\x32\x0f.script.Message"I\n\x0bRecvRequest\x12\x0f\n\x07peer_id\x18\x01 \x02(\t\x12\x1c\n\x03msg\x18\x02 \x02(\x0b\x32\x0f.script.Message\x12\x0b\n\x03seq\x18\x03 \x02(\x04"7\n\x08Response\x12\x0e\n\x04\x64\x61ta\x18\x01 \x01(\x0cH\x00\x12\x0f\n\x05\x65rror\x18\x02 \x01(\tH\x00\x42\n\n\x08response"?\n\x0cSendResponse\x12\x0b\n\x03seq\x18\x01 \x02(\x04\x12"\n\x08response\x18\x02 \x02(\x0b\x32\x10.script.Response"\x17\n\x04Peer\x12\x0f\n\x07peer_id\x18\x01 \x02(\t"$\n\x05Peers\x12\x1b\n\x05peers\x18\x01 \x03(\x0b\x32\x0c.script.Peer"z\n\x0eNeighbourEvent\x12\x1d\n\x04init\x18\x01 \x01(\x0b\x32\r.script.PeersH\x00\x12"\n\ndiscovered\x18\x02 \x01(\x0b\x32\x0c.script.PeerH\x00\x12\x1c\n\x04lost\x18\x03 \x01(\x0b\x32\x0c.script.PeerH\x00\x42\x07\n\x05\x65vent"V\n\x11MeshTopologyEvent\x12\x1a\n\x04peer\x18\x01 \x02(\x0b\x32\x0c.script.Peer\x12%\n\x05\x65vent\x18\x02 \x02(\x0b\x32\x16.script.NeighbourEvent" \n\x12GossipSubMessageID\x12\n\n\x02id\x18\x01 \x02(\x0c">\n\x10GossipSubMessage\x12\x0c\n\x04\x64\x61ta\x18\x01 \x02(\x0c\x12\x1c\n\x05topic\x18\x02 \x02(\x0b\x32\r.script.Topic"z\n\x14GossipSubRecvMessage\x12\x0f\n\x07peer_id\x18\x01 \x02(\t\x12%\n\x03msg\x18\x02 \x02(\x0b\x32\x18.script.GossipSubMessage\x12*\n\x06msg_id\x18\x03 \x02(\x0b\x32\x1a.script.GossipSubMessageID"3\n\x06\x44HTKey\x12\x1c\n\x05topic\x18\x01 \x02(\x0b\x32\r.script.Topic\x12\x0b\n\x03key\x18\x02 \x02(\x0c":\n\x0c\x44HTPutRecord\x12\x1b\n\x03key\x18\x01 \x02(\x0b\x32\x0e.script.DHTKey\x12\r\n\x05value\x18\x02 \x02(\x0c":\n\x0c\x44HTGetRecord\x12\x1b\n\x03key\x18\x01 \x02(\x0b\x32\x0e.script.DHTKey\x12\r\n\x05value\x18\x02 \x01(\x0c"\x18\n\x08\x46ilePath\x12\x0c\n\x04path\x18\x01 \x02(\t"+\n\x03\x43ID\x12\x0c\n\x04hash\x18\x01 \x02(\x0c\x12\x16\n\x02id\x18\x02 \x02(\x0b\x32\n.script.ID2\xa9\x01\n\x07ReqResp\x12/\n\x04Send\x12\x13.script.SendRequest\x1a\x10.script.Response"\x00\x12;\n\x04Recv\x12\x1a.script.OptionalTopicQuery\x1a\x13.script.RecvRequest"\x00\x30\x01\x12\x30\n\x07Respond\x12\x14.script.SendResponse\x1a\r.script.Empty"\x00\x32t\n\tDiscovery\x12<\n\x0fSubscribeEvents\x12\r.script.Empty\x1a\x16.script.NeighbourEvent"\x00\x30\x01\x12)\n\x08GetOwnId\x12\r.script.Empty\x1a\x0c.script.Peer"\x00\x32\x8c\x01\n\tGossipSub\x12<\n\tSubscribe\x12\r.script.Topic\x1a\x1c.script.GossipSubRecvMessage"\x00\x30\x01\x12\x41\n\x07Publish\x12\x18.script.GossipSubMessage\x1a\x1a.script.GossipSubMessageID"\x00\x32\xcc\x01\n\x03\x44HT\x12\x32\n\tPutRecord\x12\x14.script.DHTPutRecord\x1a\r.script.Empty"\x00\x12\x33\n\tGetRecord\x12\x0e.script.DHTKey\x1a\x14.script.DHTGetRecord"\x00\x12*\n\x07Provide\x12\x0e.script.DHTKey\x1a\r.script.Empty"\x00\x12\x30\n\x0cGetProviders\x12\x0e.script.DHTKey\x1a\x0c.script.Peer"\x00\x30\x01\x32j\n\x0c\x46ileTransfer\x12.\n\x0bPublishFile\x12\x10.script.FilePath\x1a\x0b.script.CID"\x00\x12*\n\x07GetFile\x12\x0b.script.CID\x1a\x10.script.FilePath"\x00\x32N\n\x05\x44\x65\x62ug\x12\x45\n\x15SubscribeMeshTopology\x12\r.script.Empty\x1a\x19.script.MeshTopologyEvent"\x00\x30\x01'
-)
+
+
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0cscript.proto\x12\x06script\"\x07\n\x05\x45mpty\"\x12\n\x02ID\x12\x0c\n\x04ulid\x18\x01 \x02(\t\"\x16\n\x05Topic\x12\r\n\x05topic\x18\x01 \x02(\t\"-\n\rOptionalTopic\x12\x1c\n\x05topic\x18\x01 \x01(\x0b\x32\r.script.Topic\"F\n\nTopicQuery\x12\x1e\n\x05topic\x18\x01 \x01(\x0b\x32\r.script.TopicH\x00\x12\x0f\n\x05regex\x18\x02 \x01(\tH\x00\x42\x07\n\x05query\"7\n\x12OptionalTopicQuery\x12!\n\x05query\x18\x01 \x01(\x0b\x32\x12.script.TopicQuery\"=\n\x07Message\x12\x0c\n\x04\x64\x61ta\x18\x01 \x02(\x0c\x12$\n\x05topic\x18\x02 \x02(\x0b\x32\x15.script.OptionalTopic\"<\n\x0bSendRequest\x12\x0f\n\x07peer_id\x18\x01 \x02(\t\x12\x1c\n\x03msg\x18\x02 \x02(\x0b\x32\x0f.script.Message\"I\n\x0bRecvRequest\x12\x0f\n\x07peer_id\x18\x01 \x02(\t\x12\x1c\n\x03msg\x18\x02 \x02(\x0b\x32\x0f.script.Message\x12\x0b\n\x03seq\x18\x03 \x02(\x04\"7\n\x08Response\x12\x0e\n\x04\x64\x61ta\x18\x01 \x01(\x0cH\x00\x12\x0f\n\x05\x65rror\x18\x02 \x01(\tH\x00\x42\n\n\x08response\"?\n\x0cSendResponse\x12\x0b\n\x03seq\x18\x01 \x02(\x04\x12\"\n\x08response\x18\x02 \x02(\x0b\x32\x10.script.Response\"\x17\n\x04Peer\x12\x0f\n\x07peer_id\x18\x01 \x02(\t\"$\n\x05Peers\x12\x1b\n\x05peers\x18\x01 \x03(\x0b\x32\x0c.script.Peer\"z\n\x0eNeighbourEvent\x12\x1d\n\x04init\x18\x01 \x01(\x0b\x32\r.script.PeersH\x00\x12\"\n\ndiscovered\x18\x02 \x01(\x0b\x32\x0c.script.PeerH\x00\x12\x1c\n\x04lost\x18\x03 \x01(\x0b\x32\x0c.script.PeerH\x00\x42\x07\n\x05\x65vent\"V\n\x11MeshTopologyEvent\x12\x1a\n\x04peer\x18\x01 \x02(\x0b\x32\x0c.script.Peer\x12%\n\x05\x65vent\x18\x02 \x02(\x0b\x32\x16.script.NeighbourEvent\" \n\x12GossipSubMessageID\x12\n\n\x02id\x18\x01 \x02(\x0c\">\n\x10GossipSubMessage\x12\x0c\n\x04\x64\x61ta\x18\x01 \x02(\x0c\x12\x1c\n\x05topic\x18\x02 \x02(\x0b\x32\r.script.Topic\"z\n\x14GossipSubRecvMessage\x12\x0f\n\x07peer_id\x18\x01 \x02(\t\x12%\n\x03msg\x18\x02 \x02(\x0b\x32\x18.script.GossipSubMessage\x12*\n\x06msg_id\x18\x03 \x02(\x0b\x32\x1a.script.GossipSubMessageID\"3\n\x06\x44HTKey\x12\x1c\n\x05topic\x18\x01 \x02(\x0b\x32\r.script.Topic\x12\x0b\n\x03key\x18\x02 \x02(\x0c\":\n\x0c\x44HTPutRecord\x12\x1b\n\x03key\x18\x01 \x02(\x0b\x32\x0e.script.DHTKey\x12\r\n\x05value\x18\x02 \x02(\x0c\":\n\x0c\x44HTGetRecord\x12\x1b\n\x03key\x18\x01 \x02(\x0b\x32\x0e.script.DHTKey\x12\r\n\x05value\x18\x02 \x01(\x0c\"\x18\n\x08\x46ilePath\x12\x0c\n\x04path\x18\x01 \x02(\t\"+\n\x03\x43ID\x12\x0c\n\x04hash\x18\x01 \x02(\x0c\x12\x16\n\x02id\x18\x02 \x02(\x0b\x32\n.script.ID2\xa9\x01\n\x07ReqResp\x12/\n\x04Send\x12\x13.script.SendRequest\x1a\x10.script.Response\"\x00\x12;\n\x04Recv\x12\x1a.script.OptionalTopicQuery\x1a\x13.script.RecvRequest\"\x00\x30\x01\x12\x30\n\x07Respond\x12\x14.script.SendResponse\x1a\r.script.Empty\"\x00\x32t\n\tDiscovery\x12<\n\x0fSubscribeEvents\x12\r.script.Empty\x1a\x16.script.NeighbourEvent\"\x00\x30\x01\x12)\n\x08GetOwnId\x12\r.script.Empty\x1a\x0c.script.Peer\"\x00\x32\x8c\x01\n\tGossipSub\x12<\n\tSubscribe\x12\r.script.Topic\x1a\x1c.script.GossipSubRecvMessage\"\x00\x30\x01\x12\x41\n\x07Publish\x12\x18.script.GossipSubMessage\x1a\x1a.script.GossipSubMessageID\"\x00\x32\xcc\x01\n\x03\x44HT\x12\x32\n\tPutRecord\x12\x14.script.DHTPutRecord\x1a\r.script.Empty\"\x00\x12\x33\n\tGetRecord\x12\x0e.script.DHTKey\x1a\x14.script.DHTGetRecord\"\x00\x12*\n\x07Provide\x12\x0e.script.DHTKey\x1a\r.script.Empty\"\x00\x12\x30\n\x0cGetProviders\x12\x0e.script.DHTKey\x1a\x0c.script.Peer\"\x00\x30\x01\x32j\n\x0c\x46ileTransfer\x12.\n\x0bPublishFile\x12\x10.script.FilePath\x1a\x0b.script.CID\"\x00\x12*\n\x07GetFile\x12\x0b.script.CID\x1a\x10.script.FilePath\"\x00\x32N\n\x05\x44\x65\x62ug\x12\x45\n\x15SubscribeMeshTopology\x12\r.script.Empty\x1a\x19.script.MeshTopologyEvent\"\x00\x30\x01')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'script_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
-    DESCRIPTOR._loaded_options = None
-    _globals['_EMPTY']._serialized_start = 24
-    _globals['_EMPTY']._serialized_end = 31
-    _globals['_ID']._serialized_start = 33
-    _globals['_ID']._serialized_end = 51
-    _globals['_TOPIC']._serialized_start = 53
-    _globals['_TOPIC']._serialized_end = 75
-    _globals['_OPTIONALTOPIC']._serialized_start = 77
-    _globals['_OPTIONALTOPIC']._serialized_end = 122
-    _globals['_TOPICQUERY']._serialized_start = 124
-    _globals['_TOPICQUERY']._serialized_end = 194
-    _globals['_OPTIONALTOPICQUERY']._serialized_start = 196
-    _globals['_OPTIONALTOPICQUERY']._serialized_end = 251
-    _globals['_MESSAGE']._serialized_start = 253
-    _globals['_MESSAGE']._serialized_end = 314
-    _globals['_SENDREQUEST']._serialized_start = 316
-    _globals['_SENDREQUEST']._serialized_end = 376
-    _globals['_RECVREQUEST']._serialized_start = 378
-    _globals['_RECVREQUEST']._serialized_end = 451
-    _globals['_RESPONSE']._serialized_start = 453
-    _globals['_RESPONSE']._serialized_end = 508
-    _globals['_SENDRESPONSE']._serialized_start = 510
-    _globals['_SENDRESPONSE']._serialized_end = 573
-    _globals['_PEER']._serialized_start = 575
-    _globals['_PEER']._serialized_end = 598
-    _globals['_PEERS']._serialized_start = 600
-    _globals['_PEERS']._serialized_end = 636
-    _globals['_NEIGHBOUREVENT']._serialized_start = 638
-    _globals['_NEIGHBOUREVENT']._serialized_end = 760
-    _globals['_MESHTOPOLOGYEVENT']._serialized_start = 762
-    _globals['_MESHTOPOLOGYEVENT']._serialized_end = 848
-    _globals['_GOSSIPSUBMESSAGEID']._serialized_start = 850
-    _globals['_GOSSIPSUBMESSAGEID']._serialized_end = 882
-    _globals['_GOSSIPSUBMESSAGE']._serialized_start = 884
-    _globals['_GOSSIPSUBMESSAGE']._serialized_end = 946
-    _globals['_GOSSIPSUBRECVMESSAGE']._serialized_start = 948
-    _globals['_GOSSIPSUBRECVMESSAGE']._serialized_end = 1070
-    _globals['_DHTKEY']._serialized_start = 1072
-    _globals['_DHTKEY']._serialized_end = 1123
-    _globals['_DHTPUTRECORD']._serialized_start = 1125
-    _globals['_DHTPUTRECORD']._serialized_end = 1183
-    _globals['_DHTGETRECORD']._serialized_start = 1185
-    _globals['_DHTGETRECORD']._serialized_end = 1243
-    _globals['_FILEPATH']._serialized_start = 1245
-    _globals['_FILEPATH']._serialized_end = 1269
-    _globals['_CID']._serialized_start = 1271
-    _globals['_CID']._serialized_end = 1314
-    _globals['_REQRESP']._serialized_start = 1317
-    _globals['_REQRESP']._serialized_end = 1486
-    _globals['_DISCOVERY']._serialized_start = 1488
-    _globals['_DISCOVERY']._serialized_end = 1604
-    _globals['_GOSSIPSUB']._serialized_start = 1607
-    _globals['_GOSSIPSUB']._serialized_end = 1747
-    _globals['_DHT']._serialized_start = 1750
-    _globals['_DHT']._serialized_end = 1954
-    _globals['_FILETRANSFER']._serialized_start = 1956
-    _globals['_FILETRANSFER']._serialized_end = 2062
-    _globals['_DEBUG']._serialized_start = 2064
-    _globals['_DEBUG']._serialized_end = 2142
+  DESCRIPTOR._loaded_options = None
+  _globals['_EMPTY']._serialized_start=24
+  _globals['_EMPTY']._serialized_end=31
+  _globals['_ID']._serialized_start=33
+  _globals['_ID']._serialized_end=51
+  _globals['_TOPIC']._serialized_start=53
+  _globals['_TOPIC']._serialized_end=75
+  _globals['_OPTIONALTOPIC']._serialized_start=77
+  _globals['_OPTIONALTOPIC']._serialized_end=122
+  _globals['_TOPICQUERY']._serialized_start=124
+  _globals['_TOPICQUERY']._serialized_end=194
+  _globals['_OPTIONALTOPICQUERY']._serialized_start=196
+  _globals['_OPTIONALTOPICQUERY']._serialized_end=251
+  _globals['_MESSAGE']._serialized_start=253
+  _globals['_MESSAGE']._serialized_end=314
+  _globals['_SENDREQUEST']._serialized_start=316
+  _globals['_SENDREQUEST']._serialized_end=376
+  _globals['_RECVREQUEST']._serialized_start=378
+  _globals['_RECVREQUEST']._serialized_end=451
+  _globals['_RESPONSE']._serialized_start=453
+  _globals['_RESPONSE']._serialized_end=508
+  _globals['_SENDRESPONSE']._serialized_start=510
+  _globals['_SENDRESPONSE']._serialized_end=573
+  _globals['_PEER']._serialized_start=575
+  _globals['_PEER']._serialized_end=598
+  _globals['_PEERS']._serialized_start=600
+  _globals['_PEERS']._serialized_end=636
+  _globals['_NEIGHBOUREVENT']._serialized_start=638
+  _globals['_NEIGHBOUREVENT']._serialized_end=760
+  _globals['_MESHTOPOLOGYEVENT']._serialized_start=762
+  _globals['_MESHTOPOLOGYEVENT']._serialized_end=848
+  _globals['_GOSSIPSUBMESSAGEID']._serialized_start=850
+  _globals['_GOSSIPSUBMESSAGEID']._serialized_end=882
+  _globals['_GOSSIPSUBMESSAGE']._serialized_start=884
+  _globals['_GOSSIPSUBMESSAGE']._serialized_end=946
+  _globals['_GOSSIPSUBRECVMESSAGE']._serialized_start=948
+  _globals['_GOSSIPSUBRECVMESSAGE']._serialized_end=1070
+  _globals['_DHTKEY']._serialized_start=1072
+  _globals['_DHTKEY']._serialized_end=1123
+  _globals['_DHTPUTRECORD']._serialized_start=1125
+  _globals['_DHTPUTRECORD']._serialized_end=1183
+  _globals['_DHTGETRECORD']._serialized_start=1185
+  _globals['_DHTGETRECORD']._serialized_end=1243
+  _globals['_FILEPATH']._serialized_start=1245
+  _globals['_FILEPATH']._serialized_end=1269
+  _globals['_CID']._serialized_start=1271
+  _globals['_CID']._serialized_end=1314
+  _globals['_REQRESP']._serialized_start=1317
+  _globals['_REQRESP']._serialized_end=1486
+  _globals['_DISCOVERY']._serialized_start=1488
+  _globals['_DISCOVERY']._serialized_end=1604
+  _globals['_GOSSIPSUB']._serialized_start=1607
+  _globals['_GOSSIPSUB']._serialized_end=1747
+  _globals['_DHT']._serialized_start=1750
+  _globals['_DHT']._serialized_end=1954
+  _globals['_FILETRANSFER']._serialized_start=1956
+  _globals['_FILETRANSFER']._serialized_end=2062
+  _globals['_DEBUG']._serialized_start=2064
+  _globals['_DEBUG']._serialized_end=2142
 # @@protoc_insertion_point(module_scope)
