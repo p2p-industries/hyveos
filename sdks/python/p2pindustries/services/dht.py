65bfa650c21678b5248d613530453d12cbec1a73
Josef Zoller <josef@walterzollerpiano.com>
2024-05-29 18:56:51 +0200

    Fix python SDK

--- sdks/python/p2pindustries/services/dht.py
+++ sdks/python/p2pindustries/services/dht.py
@@ -17,25 +17,25 @@ class DHTService:
         Puts a record in the DHT table under a specific topic
         """
         await self.stub.PutRecord(
-            DHTPutRecord(key=DHTKey(topic=Topic(topic), key=enc(key)), value=enc(value))
+            DHTPutRecord(key=DHTKey(topic=Topic(topic=topic), key=enc(key)), value=enc(value))
         )
 
     async def get_record(self, topic: str, key: str | bytes) -> DHTGetRecord:
         """
         Retrieved a record in the DHT table under a specific topic
         """
-        record = await self.stub.GetRecord(DHTKey(topic=Topic(topic), key=enc(key)))
+        record = await self.stub.GetRecord(DHTKey(topic=Topic(topic=topic), key=enc(key)))
         return record
 
     async def provide(self, topic: str, key: str | bytes) -> None:
         """
         Marks the peer as a provider of a record under a specific topic
         """
-        await self.stub.Provide(DHTKey(topic=Topic(topic), key=enc(key)))
+        await self.stub.Provide(DHTKey(topic=Topic(topic=topic), key=enc(key)))
 
     def get_providers(self, topic: str, key: str | bytes) -> ManagedStream[Peer]:
         """
         Returns an asynchronous iterator representing the providers of a record under a specific topic
         """
-        stream = self.stub.GetProviders(DHTKey(topic=Topic(topic), key=enc(key)))
+        stream = self.stub.GetProviders(DHTKey(topic=Topic(topic=topic), key=enc(key)))
         return ManagedStream(stream)
