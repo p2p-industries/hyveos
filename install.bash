93508336ddd643ea1a95f8c2414170b3b1bd57e1
Hannes Furmans <55623006+umgefahren@users.noreply.github.com>
2024-12-14 17:04:39 +0100

    Apply suggestions from code review
    Co-authored-by: Josef Zoller <josef@walterzollerpiano.com>

--- install.bash
+++ install.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Set color to green
 GREEN="\033[0;32m"
@@ -29,7 +29,7 @@ function continue_installation() {
 
 	echo
 
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
+	if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 		echo -e "${GREEN}Continuing installation${RESET}"
 	else
 		echo -e "${RED}Exiting installation${RESET}"
@@ -55,15 +55,15 @@ if [ -f /etc/os-release ]; then
 	OS=$NAME
 	VER=$VERSION_ID
 
-	if [ $OS == "Raspbian GNU/Linux" ]; then
+	if [[ "$OS" == "Raspbian GNU/Linux" ]]; then
 		echo -e "${GREEN}Raspberry Pi OS detected${RESET}"
-	elif [ $OS == "Ubuntu" ]; then
+	elif [[ "$OS" == "Ubuntu" ]]; then
 		echo -e "${GREEN}Ubuntu detected${RESET}"
 
 		# Check if version is 22.04 or 24.04
-		if [ $VER == "22.04" ]; then
+		if [[ "$VER" == "22.04" ]]; then
 			echo -e "${GREEN}Ubuntu 22.04 detected${RESET}"
-		elif [ $VER == "24.04" ]; then
+		elif [[ "$VER" == "24.04" ]]; then
 			echo -e "${GREEN}Ubuntu 24.04 detected${RESET}"
 		else
 			echo -e "${RED}Unsupported Ubuntu version${RESET}"
@@ -84,9 +84,9 @@ fi
 
 ARCH=$(uname -m)
 
-if [[ $ARCH == "aarch64" ]]; then
+if [[ "$ARCH" == "aarch64" ]]; then
 	echo -e "${GREEN}ARM64 detected${RESET}"
-elif [[ $ARCH == "x86_64" ]]; then
+elif [[ "$ARCH" == "x86_64" ]]; then
 	echo -e "${GREEN}x86_64 detected${RESET}"
 else
 	echo -e "${RED}Unsupported architecture${RESET}"
@@ -96,19 +96,13 @@ else
 	continue_installation
 fi
 
-# Check if user is root
-
-if [ "$EUID" -ne 0 ]; then
-	echo "Not root, escalating permissions"
-fi
-
 # Install dependencies
 
 read -p "Do you want to install dependencies for this script (wget, curl, gpg, jq)? (y/n) " -n 1 -r
 
 echo
 
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 	echo -e "${GREEN}Installing dependencies${RESET}"
 	sudo apt update
 	sudo apt install wget curl gpg jq
@@ -119,7 +113,7 @@ fi
 
 # Check if docker is installed (docker --version)
 
-if [ -x "$(command -v docker)" ]; then
+if [[ -x "$(command -v docker)" ]]; then
 	echo -e "${GREEN}Docker is already installed and running${RESET}"
 else
 	# Install Docker
@@ -134,24 +128,24 @@ else
 	echo -e "${RED}Docker is not installed${RESET}"
 	echo -e "${YELLOW}You have three options:${RESET}"
 	echo -e "${YELLOW}1. Install Docker yourself (link with documentation will be provided)${RESET}"
-	echo -e "${YELLOW}2. Install Docker using docker conveniance script. (easy + potentially unsafe)${RESET}"
+	echo -e "${YELLOW}2. Install Docker using docker convenience script. (easy + potentially unsafe)${RESET}"
 	echo -e "${YELLOW}3. Exit the installation${RESET}"
 
 	read -p "Choose an option (1/2/3): " -n 1 -r
 	echo
 
-	if [[ $REPLY == "1" ]]; then
+	if [[ "$REPLY" == "1" ]]; then
 		echo -e "${YELLOW}Please install Docker manually by visiting the following link: https://docs.docker.com/engine/install/${RESET}"
 		# Open the link in the browser
 		xdg-open https://docs.docker.com/engine/install/
 
 		echo -e "Return to the terminal after installing Docker"
 		read -p "Press any key to continue"
-	elif [[ $REPLY == "2" ]]; then
+	elif [[ "$REPLY" == "2" ]]; then
 		echo -e "${YELLOW}Installing Docker using the convenience script${RESET}"
-		curl -sSL https://get.docker.com | sh
+		curl -sSL https://get.docker.com | sh || (echo -e "${RED}Error encountered while installing Docker${RESET}" && exit 1)
 		echo "${GREEN}Docker installed successfully${RESET}"
-	elif [[ $REPLY == "3" ]]; then
+	elif [[ "$REPLY" == "3" ]]; then
 		echo -e "${RED}Exiting installation${RESET}"
 		exit 1
 	else
@@ -164,13 +158,13 @@ fi
 read -p "Do you want to install mesh support? (y/n) " -n 1 -r
 echo
 
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 	MESH_SUPPORT=false
 	echo -e "${GREEN}Installing mesh support${RESET}"
 
 	# Check if batctl is installed (batctl --version)
 
-	if [ -x "$(command -v batctl)" ]; then
+	if [[ -x "$(command -v batctl)" ]]; then
 		echo -e "${GREEN}Batctl is already installed${RESET}"
 	else
 		# Install batctl
@@ -180,7 +174,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 
 	# Check if batman-adv is installed (modinfo batman-adv)
 
-	if [ -x "$(command -v modinfo)" ]; then
+	if [[ -x "$(command -v modinfo)" ]]; then
 		if modinfo batman-adv &>/dev/null; then
 			echo -e "${GREEN}Batman-adv is already installed${RESET}"
 			MESH_SUPPORT=true
@@ -188,7 +182,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 			# Install batman-adv
 			echo -e "${YELLOW}The batman-adv kernel module is not installed. Very often it can be installed by installing the extra linux kernel packages.${RESET}"
 			read -p "Do you want to install the batman-adv kernel module? (y/n) via linux-modules-extra-$(uname -r) " -n 1 -r
-			if [[ $REPLY =~ ^[Yy]$ ]]; then
+			if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 				sudo apt install linux-modules-extra-$(uname -r)
 
 				if modinfo batman-adv &>/dev/null; then
@@ -204,7 +198,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 
 		# If mesh support is enabled (MESH_SUPPORT=true), activate the batman-adv kernel module
 
-		if [ $MESH_SUPPORT == true ]; then
+		if [[ "$MESH_SUPPORT" == true ]]; then
 			# Check if batman-adv is loaded (lsmod | grep batman-adv)
 
 			if lsmod | grep batman-adv &>/dev/null; then
@@ -223,7 +217,7 @@ fi
 
 # Checking if hyved is installed
 
-if [ -x "$(command -v hyved)" ]; then
+if [[ -x "$(command -v hyved)" ]]; then
 	echo -e "${GREEN}Hyved is already installed${RESET}"
 else
 	echo -e "${RED}Hyved is not installed${RESET}"
@@ -237,12 +231,12 @@ fi
 
 # TODO: Configure interfaces with hyvectl
 
-echo -e "${RED}WARNING: If you choose yes in the following step the wifi interface over which you might be connected will change it's config and you will now longer be able to connect to it conventionally. Please make sure you are connected via an alternative interface or ok with loosing access. You can still access the computer with attached monitor and keyboard or via other devices in the network.${RESET}"
+echo -e "${RED}WARNING: If you choose yes in the following step, the wifi interface over which you might be connected will change its config and you won't be able to connect to it conventionally anymore. Please make sure you are connected via an alternative interface or ok with losing access. You can still access the computer with an attached monitor and keyboard or via other devices in the network.${RESET}"
 read -p "Do you want to configure the interfaces now? (y/n) " -n 1 -r
 
 echo
 
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 	echo -e "${GREEN}Configuring interfaces${RESET}"
 	interface=$(hyvectl init-config --json | jq -r '.selected-interface')
 
@@ -251,6 +245,10 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 	sudo systemctl enable --now wpa_supplicant@$interface
 	sudo systemctl enable --now hyveos-batman@$interface
 fi
+
+echo -e "${GREEN}Enabling docker and the batman-neighbours-daemon to run at boot.${RESET}"
+
+continue_installation
 
 sudo systemctl enable --now docker
 sudo systemctl enable --now batman-neighbours-daemon
