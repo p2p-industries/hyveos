b1ada7fdf2dc109bd604813d94847e995aed5204
umgefahren <55623006+umgefahren@users.noreply.github.com>
2024-12-16 13:33:31 +0100

    Proof of Concept for client

---
+++ sdks/typescript/src/index.ts
@@ -0,0 +1,17 @@
+import {
+  ScriptingClient,
+  GossipSubClient,
+  DHTClient,
+  DBClient,
+  DebugClient,
+  DiscoveryClient,
+} from "./gen/ScriptServiceClientPb";
+
+export default {
+  ScriptingClient,
+  GossipSubClient,
+  DHTClient,
+  DBClient,
+  DebugClient,
+  DiscoveryClient,
+};
