d59ff22a7bb8a0cfe6979b668e3b4b469db94037
umgefahren <55623006+umgefahren@users.noreply.github.com>
2024-12-18 16:12:28 +0100

    Move stuff

---
+++ sdks/typescript/hyveos-sdk/dht.ts
@@ -0,0 +1,68 @@
+import type { Transport } from 'npm:@connectrpc/connect';
+import { AbortOnDispose, BaseService } from './core.ts';
+import { DHT as Service, type Peer } from './gen/script_pb.ts';
+
+export class ProvidersStream extends AbortOnDispose implements AsyncIterable<string>, Disposable {
+  stream: AsyncIterable<Peer>;
+
+  constructor(stream: AsyncIterable<Peer>, abortController: AbortController) {
+    super(abortController);
+    this.stream = stream;
+  }
+
+  async *[Symbol.asyncIterator](): AsyncIterator<string> {
+    for await (const { peerId } of this.stream) {
+      yield peerId;
+    }
+  }
+}
+
+export class DHT extends BaseService<typeof Service> {
+  public static __create(transport: Transport): DHT {
+    return new DHT(Service, transport);
+  }
+
+  public async putRecord(topic: string, key: Uint8Array, data: Uint8Array) {
+    await this.client.putRecord({
+      key: {
+        key,
+        topic: {
+          topic
+        }
+      },
+      value: {
+        data
+      }
+    });
+  }
+
+  public async getRecord(topic: string, key: Uint8Array) {
+    const { data } = await this.client.getRecord({
+      key,
+      topic: {
+        topic
+      }
+    });
+    if (!data) {
+      return null;
+    }
+    return data.data;
+  }
+
+  public async provide(key: Uint8Array) {
+    await this.client.provide({
+      key
+    });
+  }
+
+  public getProviders(key: Uint8Array) {
+    const abortController = new AbortController();
+    const stream = this.client.getProviders(
+      { key },
+      {
+        signal: abortController.signal
+      }
+    );
+    return new ProvidersStream(stream, abortController);
+  }
+}
