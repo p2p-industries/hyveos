daa1d3be9c19d07dae7e24a021d4f5a60f95ac12
umgefahren <55623006+umgefahren@users.noreply.github.com>
2024-12-18 17:19:50 +0100

    Modify to allow for UDS connections

--- sdks/typescript/base_test.ts
+++ sdks/typescript/base_test.ts
@@ -1,15 +1,20 @@
 import { Client } from 'hyveos-sdk';
-import { Connection } from 'hyveos-web';
+import { Connection } from 'hyveos-server';
 import { assertEquals } from 'jsr:@std/assert';
 
-const connection = new Connection('http://localhost:8080');
+const connection = new Connection('unix:///tmp/hyved/running');
 const client = new Client(connection);
 
-Deno.test('localdb-put-get', async () => {
-  const random_key = Math.random().toString(36).substring(7);
-  const random_value = Math.random().toString(36).substring(7);
-  const random_value_encoded = new TextEncoder().encode(random_value);
-  await client.localdb.put(random_key, random_value_encoded);
-  const value = await client.localdb.get(random_key);
-  assertEquals(value, random_value_encoded);
+Deno.test({
+  name: 'localdb-put-get',
+  async fn() {
+    const random_key = Math.random().toString(36).substring(7);
+    const random_value = Math.random().toString(36).substring(7);
+    const random_value_encoded = new TextEncoder().encode(random_value);
+    await client.localdb.put(random_key, random_value_encoded);
+    const value = await client.localdb.get(random_key);
+    assertEquals(value, random_value_encoded);
+  },
+  sanitizeOps: false,
+  sanitizeResources: false
 });
--- sdks/typescript/hyveos-server/mod.ts
+++ sdks/typescript/hyveos-server/mod.ts
@@ -1,16 +1,28 @@
-import type { Transport } from 'npm:@connectrpc/connect'
-import { createGrpcTransport } from 'npm:@connectrpc/connect-node'
-import type { ITransport } from 'hyveos-sdk'
+import type { Transport } from 'npm:@connectrpc/connect';
+import { createGrpcTransport } from 'npm:@connectrpc/connect-node';
+import type { ITransport } from 'hyveos-sdk';
+import { connect } from 'node:net';
 
 export class Connection implements ITransport {
-  private url: string
+  private url: string;
   constructor(url: string) {
-    this.url = url
+    this.url = url;
   }
 
   transport(): Transport {
+    if (this.url.startsWith('unix') || this.url.startsWith('http+unix')) {
+      return createGrpcTransport({
+        baseUrl: 'http://socket.localhost',
+        nodeOptions: {
+          createConnection: () => {
+            const url = this.url.replace('http+unix://', '').replace('unix://', '');
+            return connect(url);
+          }
+        }
+      });
+    }
     return createGrpcTransport({
-      baseUrl: this.url,
-    })
+      baseUrl: this.url
+    });
   }
 }
