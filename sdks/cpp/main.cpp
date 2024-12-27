c72ad9c55efb6e6b38f1054e78a646164c7cc4b4
umgefahren <55623006+umgefahren@users.noreply.github.com>
2024-12-27 18:33:13 +0100

    Example use

--- sdks/cpp/main.cpp
+++ sdks/cpp/main.cpp
@@ -1,6 +1,10 @@
 #include <grpcpp/grpcpp.h>
+#include <grpcpp/support/status.h>
 
 #include <memory>
+#include <optional>
+#include <stdexcept>
+#include <string>
 
 #include "script.grpc.pb.h"
 #include "script.pb.h"
@@ -9,6 +13,41 @@ class ReqRespClient {
    public:
     ReqRespClient(std::shared_ptr<grpc::Channel> channel)
         : stub_(script::ReqResp::NewStub(channel)) {}
+
+    std::string Send(std::string peer_id, std::string data,
+                     std::optional<std::string> topic) {
+        auto peer = new script::Peer();
+        peer->set_allocated_peer_id(&peer_id);
+
+        auto d = new script::Data();
+        d->set_allocated_data(&data);
+
+        auto ot = new script::OptionalTopic();
+        if (topic.has_value()) {
+            auto t = new script::Topic();
+            t->set_allocated_topic(&topic.value());
+            ot->set_allocated_topic(t);
+        }
+
+        auto message = new script::Message();
+        message->set_allocated_data(d);
+        message->set_allocated_topic(ot);
+
+        script::SendRequest request;
+        request.set_allocated_peer(peer);
+        request.set_allocated_msg(message);
+        script::Response response;
+
+        grpc::ClientContext context;
+        grpc::Status status = stub_->Send(&context, request, &response);
+        if (status.ok()) {
+            auto d = response.data();
+            return d.data();
+        } else {
+            auto error_message = status.error_message();
+            throw std::runtime_error(error_message);
+        }
+    }
 
    private:
     std::unique_ptr<script::ReqResp::Stub> stub_;
