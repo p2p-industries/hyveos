c82d68b52abf7a996da5b3f9851299fc470a5dcd
umgefahren <55623006+umgefahren@users.noreply.github.com>
2025-01-04 16:03:27 -0800

    Format correctly

--- sdks/typescript/hyveos-web/mod.ts
+++ sdks/typescript/hyveos-web/mod.ts
@@ -1,21 +1,21 @@
-import type { ITransport } from "jsr:hyveos-sdk";
-import { createGrpcWebTransport } from "npm:@connectrpc/connect-web";
-import type { Transport } from "npm:@connectrpc/connect";
+import type { ITransport } from 'jsr:hyveos-sdk'
+import { createGrpcWebTransport } from 'npm:@connectrpc/connect-web'
+import type { Transport } from 'npm:@connectrpc/connect'
 
 export class Connection implements ITransport {
-  public url: string;
+  public url: string
 
   constructor(url: string) {
-    this.url = url;
+    this.url = url
   }
 
   transport(): Transport {
     return createGrpcWebTransport({
       baseUrl: this.url,
-    });
+    })
   }
 
   isUnix(): boolean {
-    return false;
+    return false
   }
 }
