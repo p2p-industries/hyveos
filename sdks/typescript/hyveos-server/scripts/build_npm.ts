f2b1db467324b07d2dd20b299e322c2303d05754
umgefahren <55623006+umgefahren@users.noreply.github.com>
2025-01-13 19:39:59 -0800

    Fixed scripts and worked on server

--- sdks/typescript/hyveos-server/scripts/build_npm.ts
+++ sdks/typescript/hyveos-server/scripts/build_npm.ts
@@ -5,12 +5,11 @@ await emptyDir('./npm')
 await build({
   entryPoints: ['./mod.ts'],
   outDir: './npm',
-  importMap: '../deno.json',
   shims: {
     deno: true,
   },
   package: {
-    name: 'hyveos-server',
+    name: '@hyveos/server',
     version: Deno.args[0],
     description: 'HyveOS connection for the server',
     license: 'MIT',
--- sdks/typescript/hyveos-web/scripts/build_npm.ts
+++ sdks/typescript/hyveos-web/scripts/build_npm.ts
@@ -5,7 +5,6 @@ await emptyDir('./npm')
 await build({
   entryPoints: ['./mod.ts'],
   outDir: './npm',
-  importMap: '../deno.json',
   shims: {
     deno: false,
   },
@@ -13,7 +12,7 @@ await build({
     lib: ['ESNext', 'DOM'],
   },
   package: {
-    name: 'hyveos-web',
+    name: '@hyveos/web',
     version: Deno.args[0],
     description: 'HyveOS connector for the browser',
     license: 'MIT',
