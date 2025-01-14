25aa1e6aab044608fe0b7f2fddc6140909cbd619
umgefahren <55623006+umgefahren@users.noreply.github.com>
2025-01-13 18:58:29 -0800

    Correct build rs

--- crates/core/build.rs
+++ crates/core/build.rs
@@ -1,7 +1,7 @@
 use std::{env::var, path::Path};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let protos_path = Path::new(&var("CARGO_MANIFEST_DIR").unwrap()).join("../../protos");
+    let protos_path = Path::new(&var("CARGO_MANIFEST_DIR").unwrap()).join("protos");
     tonic_build::configure()
         .build_client(true)
         .compile_protos(&[protos_path.join("script.proto")], &[protos_path])?;
