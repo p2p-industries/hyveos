06da4131746c9b9f89ed24c391f8a4ee80dbb060
Josef Zoller <josef@walterzollerpiano.com>
2025-01-18 01:54:03 -0800

    Install shell completions and manpage

--- crates/hyvectl/build.rs
+++ crates/hyvectl/build.rs
@@ -1,3 +1,4 @@
+#[cfg(feature = "generate-completions")]
 use std::{
     env,
     ffi::OsStr,
@@ -6,53 +7,92 @@ use std::{
     path::PathBuf,
 };
 
+#[cfg(feature = "generate-completions")]
 use clap_complete::{generate_to, shells::Shell, Generator};
+#[cfg(feature = "generate-completions")]
 use clap_mangen::Man;
+#[cfg(feature = "generate-completions")]
+use flate2::{write::GzEncoder, Compression};
 
-fn generate_man_page() -> Result<()> {
-    let out_dir = PathBuf::from(env::var_os("OUT_DIR").expect("OUT_DIR not set"));
+#[cfg(feature = "generate-completions")]
+fn generate_man_page(out_dir: PathBuf) -> Result<()> {
     let cmd = hyvectl_commands::command::build_cli();
 
     let man = Man::new(cmd);
-    let mut buffer = Vec::new();
-    man.render(&mut buffer)?;
+    let mut encoder = GzEncoder::new(Vec::new(), Compression::best());
+    man.render(&mut encoder)?;
 
-    let file_path = out_dir.join("hyvectl.1");
+    let buffer = encoder.finish()?;
+
+    let file_path = out_dir.join("hyvectl.1.gz");
     fs::write(&file_path, &buffer)?;
 
     eprintln!("Man page generated at {file_path:?}");
     Ok(())
 }
 
-fn generate_one_completion<G, P>(generator: G, out_dir: P, bin_name: &str)
+#[cfg(feature = "generate-completions")]
+fn generate_one_completion<G, P>(generator: G, out_dir: P, bin_name: &str) -> Result<PathBuf>
 where
     G: Generator + Copy,
     P: AsRef<OsStr>,
 {
     let mut cmd = hyvectl_commands::command::build_cli();
     generate_to(generator, &mut cmd, bin_name, &out_dir)
-        .expect("clap completion generation failed");
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
 }
 
-fn generate_all_completions() {
-    let out_dir = env::var_os("OUT_DIR").expect("OUT_DIR not set");
-    let shells = [
-        Shell::Bash,
-        Shell::Elvish,
-        Shell::Fish,
-        Shell::PowerShell,
-        Shell::Zsh,
-    ];
+#[cfg(feature = "generate-completions")]
+fn generate_all_completions(out_dir: impl AsRef<OsStr>) -> Result<()> {
+    let shells = [Shell::Bash, Shell::Fish, Shell::Zsh];
 
     for shell in shells {
-        generate_one_completion(shell, &out_dir, "hyvectl");
+        let path = generate_one_completion(shell, &out_dir, "hyvectl")?;
+        eprintln!("Completion generated at {path:?}");
     }
 
-    eprintln!("Completion scripts generated at {out_dir:?}");
-}
-
-fn main() -> io::Result<()> {
-    generate_all_completions();
-    generate_man_page()?;
     Ok(())
 }
+
+#[cfg(feature = "generate-completions")]
+fn main() -> Result<()> {
+    let manifest_dir = env::var_os("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set");
+    let target_triple = env::var("TARGET").expect("TARGET not set");
+    let host_triple = env::var("HOST").expect("HOST not set");
+    let profile = env::var("PROFILE").expect("PROFILE not set");
+    let mut target_dir = PathBuf::from(manifest_dir)
+        .join("..")
+        .join("..")
+        .join("target");
+
+    if target_triple != host_triple {
+        target_dir = target_dir.join(target_triple);
+    }
+
+    let target_dir = target_dir
+        .join(profile)
+        .canonicalize()
+        .expect("target dir not found");
+    let debian_dir = target_dir.join("debian");
+    if !debian_dir.exists() {
+        fs::create_dir(&debian_dir)?;
+    }
+
+    let completions_dir = debian_dir.join("completions");
+    if !completions_dir.exists() {
+        fs::create_dir(&completions_dir)?;
+    }
+
+    generate_all_completions(completions_dir)?;
+
+    let man_page_dir = debian_dir.join("man");
+    if !man_page_dir.exists() {
+        fs::create_dir(&man_page_dir)?;
+    }
+
+    generate_man_page(man_page_dir)?;
+    Ok(())
+}
+
+#[cfg(not(feature = "generate-completions"))]
+fn main() {}
